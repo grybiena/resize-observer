dbfa0346498c0103c405a5c2bb8bb7c0cf6aef8f
f <f@grybiena.com>
2023-06-08 12:18:04 +0200

    use package template

--- flake.nix
+++ flake.nix
@@ -1,50 +1,17 @@
-{
+rec {
   description = "resize-observer";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    ps-tools.follows = "purs-nix/ps-tools";
-    purs-nix.url = "github:grybiena/purs-nix?ref=grybiena";
+    env.url = "git+ssh://git@github.com/grybiena/purescript-environment?ref=gen-overlays";  
   };
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ ];
 
-        pkgs = import nixpkgs {
-          inherit system overlays;
-          config.allowBroken = true;
-        };
-
-        ps-tools = inputs.ps-tools.legacyPackages.${system};
-        purs-nix = inputs.purs-nix { inherit system; };
-
-        package = import ./package.nix purs-nix;
- 
-        ps =
-          purs-nix.purs { inherit (package) dependencies;
-                          dir = ./.;
-                        };
-      in 
-         { packages.default =
-             purs-nix.build
-               { name = "resize-observer";
-                 src.path = ./.;
-                 info = package;
-               };
-           packages.output = ps.output {};
-           devShells.default = 
-             pkgs.mkShell
-               { packages = with pkgs; [
-                   nodejs
-                   (ps.command {}) 
-                   ps-tools.for-0_15.purescript-language-server
-                   purs-nix.esbuild
-                   purs-nix.purescript
-                 ];
-               };
-         }
+  outputs = inputs@{ env, ... }:
+    env.flake-utils.lib.eachDefaultSystem (system:
+      env.build-package { inherit system;
+                          name = description;
+                          src = ./.;
+                          overlays = { };
+                          derive-package = ./package.nix;
+                        }                
    );
 }
 
